fc37e2ff7be86b4cbf28a187a1d995752b79c7b8
Russell Graf <help.bbst@gmail.com>
2015-08-11 15:26:12 -0400

    homework 6 upload

---
+++ assets/jquerycheatsheet.js
@@ -0,0 +1,8 @@
+//Cheat Sheet for jQuery - duplicated in HTML file
+
+$("body).hide();
+
+$(document).ready(function() {
+	$("leftcolanimate").fadein(1000)
+});
+
---
+++ assets/main.js
@@ -0,0 +1,3 @@
+//Cheat Sheet for Javascript / jQuery
+
+("#page-wrap").delay(500).fadeIn(1000);
